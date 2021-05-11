d959794a9acf0500264ac7eeb8001a5951d3720d
ShubhangiShubhi12 <72823605+ShubhangiShubhi12@users.noreply.github.com>
2021-05-11 21:16:54 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,24 @@
+class createDrops{
+    constructor(x,y){
+        var options = {
+            restitution: 0.5,
+            friction: 0.01
+        }
+        this.rain = Bodies.circle(x,y,5,options);
+        this.r = 5;
+
+        World.add(world, this.rain);
+    }
+
+    updateY(){
+        if(this.rain.position.y > height){
+            Matter.Body.setPosition(this.rain, {x: random(0,windowWidth),y: random(0,windowHeight)});
+        }
+    }
+    
+    display(){
+        fill(29, 0, 198);
+        ellipseMode(RADIUS);
+        ellipse(this.rain.position.x, this.rain.position.y, this.r, this.r);
+    }
+}
